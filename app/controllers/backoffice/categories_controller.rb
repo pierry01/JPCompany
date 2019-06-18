78b0b84e3ab3029f1f56a548a596f96e50e2db71
pierry01 <pierrybm@gmail.com>
2019-06-18 04:16:22 +0000

    Cadastro de Categorias (Backoffice) Parte 1/5

---
+++ app/controllers/backoffice/categories_controller.rb
@@ -0,0 +1,9 @@
+class Backoffice::CategoriesController < ApplicationController
+  before_action :authenticate_admin!
+  
+  layout 'backoffice'
+  
+  def index
+    @categories = Category.all
+  end
+end
