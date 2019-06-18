0b81859bf0e143e91629d8e45a9774639bd0a1f6
pierry01 <pierrybm@gmail.com>
2019-06-18 04:58:54 +0000

    Cadastro de Categorias (Backoffice) Parte 3/5

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -8,14 +8,27 @@ class Backoffice::CategoriesController < ApplicationController
   end
   
   def new
+    @category = Category.new
   end
   
   def create
+    @category = Category.new(params_category)
+    if @category.save
+      redirect_to backoffice_categories_path, notice: "Categoria #{@category.description} criada!"
+    else
+      render :new
+    end
   end
   
   def edit
   end
   
   def update
+  end
+  
+  private
+  
+  def params_category
+    params.require(:category).permit(:description)
   end
 end
