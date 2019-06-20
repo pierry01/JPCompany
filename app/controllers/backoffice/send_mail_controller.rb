242543e5fd9cc1cae96353ecd84ae1d6744c5f69
pierry01 <pierrybm@gmail.com>
2019-06-20 03:29:37 +0000

    Gerenciamento de erros com Ruby

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -8,7 +8,14 @@ class Backoffice::SendMailController < ApplicationController
   end
   
   def create
-    AdminMailer.send_message(current_admin, params[:'recipient-text'], params[:'subject-text'], params[:'message-text']).deliver_now
+    begin
+      AdminMailer.send_message(current_admin, params[:'recipient-text'], params[:'subject-text'], params[:'message-text']).deliver_now
+      @notify_message = 'E-mail enviado!'
+      @notify_flag = 'success'
+    rescue
+      @notify_message = 'Erro ao enviar e-mail. Tente novamente...'
+      @notify_flag = 'error'
+    end
     
     respond_to do |format|
       format.js
