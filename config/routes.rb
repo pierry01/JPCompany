c443356593c3c8134535ca0017d14e66cf20be07
pierry01 <pierrybm@gmail.com>
2019-06-22 05:08:42 +0000

    Pesquisa simples

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   namespace :site do
     get 'home', to: 'home#index'
+    get 'search', to: 'search#ads'
     
     namespace :profile do
       resources :ads, except: :destroy
