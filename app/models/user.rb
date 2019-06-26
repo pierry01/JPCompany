a0e233ad57d6a253fda84f9f44628e28c11fd958
pierry01 <pierrybm@gmail.com>
2019-06-26 06:50:14 +0000

    Validando Nested Attributes / Validações Personalizadas

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,21 @@ class User < ActiveRecord::Base
   has_one :profile_user
   accepts_nested_attributes_for :profile_user
   
+  # Validations
+  validate :nested_attributes
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  def nested_attributes
+    if nested_attributes_blank?
+      errors.add(:base, 'É necessário preencher o nome e o sobrenome.')
+    end
+  end
+  
+  def nested_attributes_blank?
+    profile_user.first_name.blank? || profile_user.second_name.blank?
+  end
 end
