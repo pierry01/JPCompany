0c2075773f97d042a33483a9c97cf81f27012860
pierry01 <pierrybm@gmail.com>
2019-06-26 00:52:56 +0000

    Dados pessoais do User (Perfil)

--- app/controllers/site/profile/my_data_controller.rb
+++ app/controllers/site/profile/my_data_controller.rb
@@ -1,5 +1,24 @@
 class Site::Profile::MyDataController < Site::ProfileController
+  before_action :set_profile_user, only: [:edit, :update]
+  
   def edit
+  end
+  
+  def update
+    if @profile_user.update(params_profile_user)
+      redirect_to edit_site_profile_my_datum_path(current_user.id), notice: 'Dados atualizados!'
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def params_profile_user
+    params.require(:profile_user).permit(:id, :first_name, :second_name, :birthdate)
+  end
+  
+  def set_profile_user
     @profile_user = ProfileUser.find_or_create_by(user_id: current_user.id)
   end
 end
